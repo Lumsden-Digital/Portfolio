8dc7eb227929630c48235baea06677faee7d7d32
Ruaidhri Lumsden <ruaidhri.lumsden@nhs.net>
2020-09-24 21:46:52 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// function renderProfile() {
+//     const getProfile = document.getElementById('main-img')
+//     getProfile.innerHTML = 
+
+//     "<div class='profile'><h2 class='name'>Ruaidhri Lumsden</h2><h2 class='profession'>Analyst / Developer</h2><a href=#'proj-container'><h3 class='btn'>Projects <i class='fas-fa-chevron-down'></i></h3></a>-</div>"
+// }
+
+// renderProfile()
